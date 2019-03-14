8401642a124a11c7bae0281db2e20c4d3859f3ea
JulioVieira <juliokaetanovieira@gmail.com>
2019-03-13 22:18:54 -0300

    Adicionado a pasta Aula110319

---
+++ Aula110319/app/src/main/java/br/edu/unifcv/faculdade/constants/ConversorMoedasConstants.java
@@ -0,0 +1,7 @@
+package br.edu.unifcv.faculdade.constants;
+
+public class ConversorMoedasConstants {
+    public static final String COTACAO_DOLAR = "cotacao_dollar";
+    public static final String COTACAO_EURO = "cotacao_euro";
+    public static final String VALOR_REAL = "valor_real";
+}
